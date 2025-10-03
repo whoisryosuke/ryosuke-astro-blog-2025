2a12924dd4abc04e5a771eb37a9e995cfb66ed11
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-10-03 13:51:32 -0700

    :lipstick: root layout + fonts + base tokens

---
+++ src/components/Fonts.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {};
+
+const Fonts = (props: Props) => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Schibsted+Grotesk:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet"
+      ></link>
+    </>
+  );
+};
+
+export default Fonts;
