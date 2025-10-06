f81a74b4098d57dd53aadc8bb7a184d1b970b772
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-10-06 15:08:36 -0700

    :zap: resize logic

--- src/components/page/Frontpage/ArtWall/ArtWallGrid.tsx
+++ src/components/page/Frontpage/ArtWall/ArtWallGrid.tsx
@@ -2,9 +2,26 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import ArtWallTile from "./ArtWallTile";
 import type { SelectedTile } from "./types";
 
+const getTileWidth = (windowWidth: number) => {
+  let tileWidth = 150;
+
+  if (windowWidth > 1000) {
+    tileWidth = 150;
+  }
+  if (windowWidth > 1600) {
+    tileWidth = 175;
+  }
+
+  return tileWidth;
+};
+
 type Props = {};
 
 const ArtWallGrid = (props: Props) => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
   const [selectedTiles, setSelectedTiles] = useState<SelectedTile[]>([
     {
       row: 0,
@@ -12,15 +29,42 @@ const ArtWallGrid = (props: Props) => {
     },
   ]);
   const interval = useRef<ReturnType<typeof setInterval>>(null);
+  // We always render 3 rows
   const rows = useMemo(() => new Array(3).fill(0), []);
-  const gridRow = useMemo(() => new Array(10).fill(0), []);
+  // We change the number of tiles per grid row based on window size
+  const gridRow = useMemo(
+    () =>
+      new Array(
+        Math.round(windowSize.width / getTileWidth(windowSize.width))
+      ).fill(0),
+    [windowSize]
+  );
 
+  // Resize logic
+  // @TODO: Throttle
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Animation logic
   const idleAnimation = () => {
     console.log("animating tiles...");
     setSelectedTiles((prevTiles) => {
       const newTiles = prevTiles.map((tile) => {
         const newTileId = tile.id + 1;
-        if (newTileId > 10) {
+        if (newTileId > gridRow.length) {
           let newRowId = tile.row + 1;
           if (newRowId > 2) newRowId = 0;
           return {
@@ -45,9 +89,10 @@ const ArtWallGrid = (props: Props) => {
     return () => {
       interval.current && clearInterval(interval.current);
     };
-  }, []);
+  }, [windowSize]);
 
   console.log("selected", selectedTiles);
+  console.log("window size", windowSize);
 
   const renderRows = rows.map((_, rowIndex) => (
     <div className="row">
