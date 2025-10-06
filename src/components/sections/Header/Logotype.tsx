8db1c5c153380399f8679eafd590c4673d0e4294
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-10-06 16:10:32 -0700

    :lipstick: logo hover transition

--- src/components/sections/Header/Logotype.tsx
+++ src/components/sections/Header/Logotype.tsx
@@ -1,15 +1,57 @@
+import { motion, stagger } from "motion/react";
 import React from "react";
+
+const containerAnimation = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  hover: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const itemAnimation = {
+  hidden: { opacity: 0, color: "var(--color-black)" },
+  show: { opacity: 1, color: "var(--color-black)" },
+  hover: { opacity: 1, color: "var(--color-primary)" },
+};
 
 type Props = {};
 
 const Logotype = (props: Props) => {
+  // const transition = {
+  //   delayChildren: stagger(0.1),
+  // };
+
+  const letters = "Ryosuke".split("").map((letter, letterIndex) => (
+    <motion.span
+      variants={itemAnimation}
+      // whileHover={{
+      //   color: "var(--color-primary)",
+      // }}
+      // transition={{
+      //   delay: 0.01 * letterIndex,
+      // }}
+    >
+      {letter}
+    </motion.span>
+  ));
   return (
-    <a href="#" className="Logotype">
-      Ryosuke
-      <div>
-        <span>Ryosuke</span>
-      </div>
-    </a>
+    <motion.a
+      variants={containerAnimation}
+      initial="hidden"
+      animate="show"
+      whileHover="hover"
+      className="Logotype"
+      href="#"
+    >
+      {letters}
+    </motion.a>
   );
 };
 
