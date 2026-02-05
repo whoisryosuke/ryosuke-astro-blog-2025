81fca197f4c5f7d6f890717833c8900fa2711bee
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-02-04 17:12:06 -0800

    :construction: better math

--- src/components/page/Frontpage/FrontpageSlider/TitleSlider.tsx
+++ src/components/page/Frontpage/FrontpageSlider/TitleSlider.tsx
@@ -1,13 +1,44 @@
 import type { CollectionEntry } from "astro:content";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type ProjectTitle = {
+  id: string;
+  title: string;
+};
 
 type Props = {
   projects: CollectionEntry<"projects">[];
   selectedProjectIndex: number;
 };
 
-const TitleSlider = ({ projects }: Props) => {
-  return <div>TitleSlider</div>;
+const TitleSlider = ({ projects, selectedProjectIndex }: Props) => {
+  const [titles, setTitles] = useState<ProjectTitle[]>([]);
+
+  useEffect(() => {
+    // Spawn initial titles
+    // We want 3 in front and 3 behind current
+    const nextTitles = new Array(6).fill(0).map((_, index) => {
+      const offsetIndex = index - 3;
+      const nextIndex = selectedProjectIndex - offsetIndex + 1;
+      const projectIndex = (nextIndex + projects.length) % projects.length;
+      console.log("index", projectIndex);
+      const nextTitle = projects[projectIndex];
+      return {
+        id: nextTitle.id,
+        title: nextTitle.data.title,
+      };
+    });
+
+    setTitles(nextTitles);
+  }, []);
+
+  return (
+    <div>
+      {titles.map((title) => (
+        <button>{title.title}</button>
+      ))}
+    </div>
+  );
 };
 
 export default TitleSlider;
