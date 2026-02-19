88bf3e06609d5943690b03fcc5ce3ce0395a71e7
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-02-19 15:03:27 -0800

    :bug: fix links on tagged pages

--- src/components/page/Frontpage/SelectedBlogs/SelectedBlogListItem.tsx
+++ src/components/page/Frontpage/SelectedBlogs/SelectedBlogListItem.tsx
@@ -2,9 +2,10 @@ import type { SelectedBlogData } from "../../../../data/selected-blogs";
 import FileIcon from "../../../icons/FileIcon";
 
 const SelectedBlogListItem = ({ title, url }: SelectedBlogData) => {
+  console.log("urrl", url);
   return (
     <li className="SelectedBlogListItem">
-      <a href={url}>
+      <a href={`/blog/${url}`}>
         <FileIcon />
         <h3>{title}</h3>
       </a>
