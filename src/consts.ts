dbc95a3ea869927c5632fd414c84103caa2c727e
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-04-15 10:05:27 -0700

    :pencil: gotta put it somewhere better

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,2 @@
 export const SITE_TITLE = "Ryosuke's Portfolio and Blog";
-export const SITE_DESCRIPTION =
-  "The blog and portfolio of Ryosuke Hana (Oscar Diaz)";
+export const SITE_DESCRIPTION = "The blog and portfolio of Ryosuke Hana";
